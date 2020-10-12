53083f820ef118120e4b72f4da72e7bbdc2db0aa
atiholoz <andre.tikholoz@s-telematics.ru>
2020-10-13 00:20:33 +0300

    *

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+
 const { connect: dbConnection, sequelize } = require('./config');
-const { User } = require('./models');
+const birds = require('./router/controllers/auth.controller');
+const swaggerDocument = require('./swagger-ui.json');
 
 const app = express();
 const PORT = process.env.APP_HOST || 3000;
+const BASE_API_URL = `/${process.env.APP_API_VERSION}/api/uis-dashboard-service`;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Tikholoz A. UIS - 411. [SERVICE]');
 });
+
+app.use(`${BASE_API_URL}/auth-controller`, birds);
 
 (async (schema, options) => {
   try {
