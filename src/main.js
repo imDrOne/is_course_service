f16a25006991255b65c6f0649c26123efc6fa1f1
atiholoz <andre.tikholoz@s-telematics.ru>
2020-10-15 12:16:49 +0300

    some comments

--- src/main.js
+++ src/main.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 
+// Init app express
+const app = express();
+
+// Data Base
 const { DB } = require('./config');
-const birds = require('./router/controllers/auth.controller');
+const auth = require('./router/controllers/auth.controller');
+
+// Swagger
 const swaggerDocument = require('./swagger-ui.json');
 
-const app = express();
+// Vars
 const PORT = process.env.PORT || 3000;
 const BASE_API_URL = `/${process.env.APP_API_VERSION}/api/uis-dashboard-service`;
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+// Routes
 app.get('/', (req, res) => {
   res.send('Tikholoz A. UIS - 411. [SERVICE]');
-  res.json(req);
 });
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(`${BASE_API_URL}/auth-controller`, auth);
+app.use(`${BASE_API_URL}/user-controller`, auth);
 
-app.use(`${BASE_API_URL}/auth-controller`, birds);
 
+// Start service
 (async () => {
   try {
     await DB.connect();
